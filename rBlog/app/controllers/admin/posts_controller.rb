55d72b73a38fc495cddc751a838f4e5d85e444d4
David Ruiz de la Rosa <david.ruizdelarosa@gmail.com>
2018-02-16 21:38:41 +0100

    Admin Resources CRUD

--- rBlog/app/controllers/admin/posts_controller.rb
+++ rBlog/app/controllers/admin/posts_controller.rb
@@ -1,22 +1,49 @@
 class Admin::PostsController < Admin::ApplicationController
   def new
+    @page_title="New Post"
+    @post=Post.new
   end
 
   def create
+    @post=Post.new(post_params)
+    if @post.save
+      flash[:notice]="Post Created"
+      redirect_to admin_posts_path
+    else
+      render "new"
+    end
   end
 
   def edit
+    @post=Post.find(params[:id])
   end
 
   def update
+    @post=Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:notice]="Post Updated"
+      redirect_to admin_posts_path
+    else
+      render "new"
+    end
   end
 
   def destroy
-  end
-
-  def index
+    @post=Post.find(params[:id])
+    @post.destroy
+    flash[:notice]="Post Removed"
+    redirect_to admin_posts_path
   end
 
   def show
   end
+
+  def index
+    @posts=Post.all
+  end
+
+  private
+    def post_params
+      params.required(:post).permit(:title,:category_id,:user_id,:tags,:body,:image)
+    end
 end
